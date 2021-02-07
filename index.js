6b98f6a0ac0c0c34dca23eda41978daebbadb7dd
kharioki <antolisah@gmail.com>
2021-02-07 17:50:18 +0300

    lists amd listfields

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const { Keystone } = require('@keystonejs/keystone');
 const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
 const { GraphQLApp } = require('@keystonejs/app-graphql');
 
+const { Text } = require('@keystonejs/fields');
+
 const keystone = new Keystone({
   adapter: new MongooseAdapter({ mongoUri: 'mongodb://localhost/keystone' }),
+});
+
+keystone.createList('Todo', {
+  fields: {
+    name: { type: Text },
+  },
 });
 
 module.exports = {
